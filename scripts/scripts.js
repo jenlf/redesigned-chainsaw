e25f00e431e095efde2b7794e8b2fe60c7461dfd
jenlf <fritz.jenniferlynn@gmail.com>
2023-06-02 16:24:09 -0400

    fixed the fade in transitions to better javascript, added the social logos, still need delayed face on those.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,32 @@
-$(document).on("scroll", function() {
-    var pageTop = $(document).scrollTop();
-    var pageBottom = pageTop + $(window).height();
-    var tags = $(".text-scroll");
+// $(document).on("scroll", function() {
+//     var pageTop = $(document).scrollTop();
+//     var pageBottom = pageTop + $(window).height();
+//     var tags = $(".text-scroll");
   
-    for (var i = 0; i < tags.length; i++) {
-      var tag = tags[i];
-      if ($(tag).position().top < pageBottom) {
-        $(tag).addClass("visible");
-      } else {
-        $(tag).removeClass("visible");
-      }
+//     for (var i = 0; i < tags.length; i++) {
+//       var tag = tags[i];
+//       if ($(tag).position().top < pageBottom) {
+//         $(tag).addClass("visible");
+//       } else {
+//         $(tag).removeClass("visible");
+//       }
+//     }
+//   });
+  
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) 
+    {
+      entry.target.classList.add('show');
+    }
+    else
+    {
+      entry.target.classList.remove('show');
     }
   });
-  
-  
+});
+
+const hiddenSections = document.querySelectorAll('.scroll-section');
+//tell the observer what to observe
+hiddenSections.forEach((el) => observer.observe(el));
